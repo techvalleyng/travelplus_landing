12e537ac5d744e9b316f81be63a4babba15beff1
Abbas Usman Idris <abbasusman0009@gmail.com>
2024-09-17 15:45:23 +0100

    added tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Home from "./pages/Home"
 
 function App() {
   
   return (
     <>
+      <Home/>
+      <div className="conatiner bg-black">
         
+      </div>
     </>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+export default function Header() {
+  return (
+    <div>
+      <div className="container-fluid flex bg-green-500">
+        <div className="flex-1">
+          <img src="" alt="" srcset="" />
+        </div>
+        <div className="flxe-1">
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="#">Book Your</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
