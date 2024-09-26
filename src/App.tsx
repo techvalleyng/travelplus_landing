65bec307c1f9ebaf5efab399b29dc1fc128b5758
Abbas Usman Idris <abbasusman0009@gmail.com>
2024-09-26 07:29:37 +0100

    added react-router-dom libaries and NotFound page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import NotFound from "./components/NotFound"
 import Home from "./pages/Home"
-
+import  {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 function App() {
   
   return (
     <>
-      <Home/>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+    </Router>
     </>
   )
 }
