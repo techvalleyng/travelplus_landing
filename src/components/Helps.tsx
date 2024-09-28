54c307fa1ddf81ec1f2cb6766433d224841bb92b
Abbas Usman Idris <abbasusman0009@gmail.com>
2024-09-28 23:13:21 +0100

    added transition to help component

--- src/components/Helps.tsx
+++ src/components/Helps.tsx
@@ -10,15 +10,15 @@ export default function Helps() {
                 <h1 className="text-gray-500 mb-4 font-inter font-bold text-6xl">have a question&#63;</h1>
             </div>
             <div className="flex mt-10 capitalize gap-20 items-center justify-center">
-                <div className="flex  flex-col  items-center border w-[200px] h-[200px] border-gray-100 shadow-lg rounded-lg font-inter">
+                <div className="flex  flex-col transition transform duration-300 ease-in-out   items-center border w-[200px] h-[200px] border-gray-100 shadow-lg rounded-lg font-inter hover:-translate-y-4">
                     <img className='w-14' src={maps} alt="" />
                     <p className="text-md ml-6 mt-4 font-bold text-gray-500">No28, ali akilu road, opp <br />kaduna <br /> investment <br /> house</p>
                 </div>
-                <div className="flex flex-col font-inter  items-center border w-[200px] h-[200px] border-gray-100 shadow-lg rounded-lg">
+                <div className="flex flex-col font-inter  items-center border w-[200px] h-[200px] border-gray-100 transition transform duration-300 ease-in-out shadow-lg rounded-lg hover:-translate-y-4">
                     <img className='w-14' src={call} alt="" />
                     <p className="text-md ml-6 mt-4 font-bold text-gray-500">call us<br /> 08025734353  08036400647</p>
                 </div>
-                <div className="flex flex-col font-inter  items-center border w-[200px] h-[200px] border-gray-100 shadow-lg rounded-lg text-gray-500">
+                <div className="flex flex-col font-inter  items-center border w-[200px] h-[200px] border-gray-100 transition transform duration-300 ease-in-out shadow-lg rounded-lg text-gray-500 hover:-translate-y-4">
                     <img className='w-14' src={mail} alt="" />
                     <p className="text-md ml-6 mt-4 font-bold">mail us&#64; travelplus&#64; gmail.com</p>
                 </div>
