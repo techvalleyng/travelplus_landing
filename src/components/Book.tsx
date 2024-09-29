02ac75f26995943969c0fdb4341de6a8f8fce274
Abbas Usman Idris <abbasusman0009@gmail.com>
2024-09-29 03:47:52 +0100

    added responsive for Book component

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,48 +1,46 @@
-import './css/book.css'
-import LocationOnIcon from '@mui/icons-material/LocationOn'
-import CallIcon from '@mui/icons-material/Call'
-import AccessTimeIcon from '@mui/icons-material/AccessTime'
+import './css/book.css';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 export default function Book() {
   return (
     <div id='Book'>
-      <div className="container-fluid font-inter  mt-8 h-[400px]  bg-cover bg-no-repeat bg-img">
-      <div className="bg-green-700 h-[400px] bg-opacity-70">
-        <div className="text-center font-interr font-extrabold  text-white capitalize">
-            <p className='text-2xl translate-y-6 font-inter'>enjoy a Comfortable trip</p>
-            <h1 className='text-6xl mt-10 ml-10 font-inter'>Book a ride Now</h1>
-        </div>
-        <form action="">
-            <div className="flex gap-8 flex-wrap justify-center font-inter items-center mt-14">
-                <div className='capitalize  relative'>
-                    <LocationOnIcon className="absolute top-4  left-2"/>
-                    <select className='py-4 pl-10 font-inter bg-white rounded  w-[200px] focus:outline-none focus:ring-2 focus:ring-green-500' name="destination" aria-label='Start Destination' id="">
-                        <option value="" disabled selected>Start Destination</option>
-                        <option value="">Kaduna</option>
-                        <option value="">Abuja</option>
-                        <option value="">Kano</option>
-                    </select>
-                </div>
-                <div className='capitalize relative font-inter'>
-                    <LocationOnIcon className="absolute top-4  left-2"/>
-                    <select className='py-4 pl-10 bg-white rounded w-[200px] focus:outline-none focus:ring-2 focus:ring-green-500' name="destination" aria-label='End Destination' id="">
-                        <option value="" disabled selected>End Destination</option>
-                        <option value="Kaduna">Kaduna</option>
-                        <option value="Kano">Kano</option>
-                        <option value="Kano">Abuja - Gusau</option>
-                        <option value="Kano">Abuja - Kusau</option>
-                    </select>
-                </div>
-                <div className='relative'>
-                    <input className='appearance-none pl-2 rounded  py-4 text-center focus:outline-none w-[200px]' type="datetime-local" aria-label='time' placeholder=''/>
-                </div>
+      <div className="container-fluid font-inter mt-8 h-[880px] md:h-[400px] md:bg-cover bg-no-repeat  bg-img">
+        <div className="bg-green-700 md:h-[400px] h-[480px] bg-opacity-70 flex flex-col justify-center">
+          <div className="text-center font-extrabold text-white capitalize">
+            <p className='text-2xl mt-10 md:text-3xl md:-translate-y-4'>Enjoy a Comfortable Trip</p>
+            <h1 className='text-4xl md:text-6xl mt-4 md:mt-2'>Book a Ride Now</h1>
+          </div>
+          <form>
+            <div className="flex flex-col md:flex-row gap-6 flex-wrap justify-center font-inter items-center mt-8">
+              <div className='relative capitalize w-full max-w-xs'>
+                <LocationOnIcon className="absolute top-4 left-3 text-gray-400" />
+                <select className='py-4 pl-10 pr-3 bg-white rounded w-full focus:outline-none focus:ring-2 focus:ring-green-500' name="destination" aria-label='Start Destination'>
+                  <option value="" disabled selected>Start Destination</option>
+                  <option value="Kaduna">Kaduna</option>
+                  <option value="Abuja">Abuja</option>
+                  <option value="Kano">Kano</option>
+                </select>
+              </div>
+              <div className='relative capitalize w-full max-w-xs'>
+                <LocationOnIcon className="absolute top-4 left-3 text-gray-400" />
+                <select className='py-4 pl-10 pr-3 bg-white rounded w-full focus:outline-none focus:ring-2 focus:ring-green-500' name="destination" aria-label='End Destination'>
+                  <option value="" disabled selected>End Destination</option>
+                  <option value="Kaduna">Kaduna</option>
+                  <option value="Kano">Kano</option>
+                  <option value="Abuja-Gusau">Abuja - Gusau</option>
+                  <option value="Abuja-Kusau">Abuja - Kusau</option>
+                </select>
+              </div>
+              <div className='relative w-full max-w-xs'>
+                <input className='appearance-none pl-2 pr-3 rounded py-4 text-center focus:outline-none w-full bg-white' type="datetime-local" aria-label='time' />
+              </div>
             </div>
             <div className='mt-8 text-center font-inter'>
-                <button className="capitalize mx-16 text-white bg-green-500 py-2 px-10 font-extrabold text-md rounded border border-white" type="submit">Book my ride</button>
+              <button className="capitalize mx-auto text-white bg-green-500 py-2 px-10 font-extrabold text-md rounded border border-white hover:bg-green-600 transition duration-150" type="submit">Book My Ride</button>
             </div>
-        </form>
-      </div>
+          </form>
+        </div>
       </div>
     </div>
-  )
+  );
 }
