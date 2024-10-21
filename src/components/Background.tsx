c6a540b2ec472def7bdfaac8f1d6f7b645c43736
Abbas Usman Idris <abbasusman0009@gmail.com>
2024-10-21 13:50:27 +0100

    update

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,14 +1,21 @@
 import AppleIcon from '@mui/icons-material/Apple';
 import Playstore from '../assets/playstore.svg';
 import hand from '../assets/hand.png';
-// import './css/background.css';
+import { useState } from 'react';
+import './css/background.css';
 import frame from '../assets/frame.png'
 import person from '../assets/person.svg'
-import arrow from '../assets/arrow.svg'
-import date from '../assets/date.svg'
+import arrow from '../assets/arrow2.svg'
+// import date from '../assets/date.svg'
 import { NavLink } from 'react-router-dom';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+
 
 export default function Background() {
+  const [startDate, setStartDate] = useState<Date | null>(null);
+
   return (
     <>
     <div className="flex flex-col md:flex-row justify-around space-y-10 font-inter">
@@ -23,21 +30,64 @@ export default function Background() {
       </div>
     </div>
 
-    <div className="md:border md:mr-16 md:ml-24 md:mt-8 md:mb-20 md:border-gray-100 rounded-md md:shadow text-[#282626] flex flex-col  md:flex-row md:items-center md:justify-between md:text-xl font-inter md:p-2">
-      <div className='flex mr-10 ml-10 md:ml-0 md:mr-0 p-4 md:p-0 border border-gray-100 rounded shadow md:shadow-none md:border-0 md:border-gray-0  relative mt-4 md:mt-0 flex-row md:space-x-40'>
-      <span className='capitalize'>from</span>
-      <img className='min-w-8 absolute right-2 md:-right-36' src={arrow} alt="" />
-      </div>
-      <div className='flex mr-10 ml-10 md:ml-0 md:mr-0 p-4  md:p-0 mt-4  flex-row md:space-x-40 shadow md:shadow-none border border-gray-100 relative rounded md:border-0 md:border-gray-0 mb-4'>
-      <span>To</span>
-      <div className='flex  flex-row space-x-2'>
-        <img className='min-w-6 absolute  md:right-12 right-2' src={date} alt=""/>
-        <span>Date</span>
-      </div>
-      </div>
-      <button className='bg-green-700 mr-10 ml-10 md:ml-0 md:mr-0 p-4 mb-10 md:mb-0 rounded text-sm text-white md:px-8 md:py-2 hover:bg-opacity-85  transition duration-300 ease-linear'>Schedule Now</button>
+    <div className='mt-4 mb-8'>
+      <form className='flex flex-col md:flex-row justify-center items-center'>
+        <div className="relative  w-full rounded-lg md:rounded-r-none md:rounded-l-lg mb-6 shadow-md border border-r h-16 border-gray-100  ml-3 mr-2  md:ml-0  md:mr-0 md:w-80">
+          <input 
+            type="text" 
+            aria-label="from" 
+            className="peer h-10  w-full ml-3 mt-2 text-gray-900 placeholder-transparent focus:outline-none border-none"
+            placeholder=" "
+          />
+          <label 
+            className="absolute left-0 -top-3.5 ml-3 mt-3.5  text-gray-500 text-sm transition-all transform -translate-y-1 peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:-top-3.5 peer-focus:text-[10px] peer-focus:text-gray-500">
+            From
+          </label>
+          <img className='absolute transform rotate-90 top-16 md:rotate-0 md:right-0 right-2 z-10 md:top-5 w-8 md:w-6 border rounded-full border-green-700'src={arrow} alt=""/>
+        </div>
+        <div className="h-16 relative w-full rounded-lg md:rounded-none mb-6 shadow-md border border-l  border-gray-100    
+          md:w-80">
+          <input 
+            aria-label='To'
+            type="text" 
+            className="peer h-10 ml-3 mt-2 w-full border-none text-gray-900 placeholder-transparent focus:outline-none"
+            placeholder=" "
+          />
+          <label 
+            className="absolute left-0 -top-3.5 ml-3 mt-3.5 text-gray-500 text-sm transition-all transform -translate-y-1 peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:-top-3.5 peer-focus:text-[10px] peer-focus:text-gray-400">
+            To
+          </label>
+        </div>
+        <div className="h-16 relative w-full rounded-lg md:rounded-none mb-6 shadow-md border border-l  border-gray-100    
+          md:w-40">
+          {/* <input 
+            type="text" 
+            id="from" 
+            className="peer h-10  ml-2 w-full md:w-36 mt-2  border-none text-gray-900 placeholder-transparent focus:outline-none "
+            placeholder=" "
+          /> */}
+           <DatePicker
+              className="peer h-10  ml-1 w-full md:w-36 mt-2  border-none text-gray-900 placeholder-transparent focus:outline-none"
+              selected={startDate}
+              onChange={(date) => setStartDate(date )}
+            />
+          <label  
+            className="absolute left-0 -top-3.5 ml-2 mt-3.5 text-gray-500 text-sm transition-all transform -translate-y-1 peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:-top-3.5 peer-focus:text-[10px] peer-focus:text-gray-400">
+            Date
+          </label>
+        </div>
+        <div className='h-16 w-full md:rounded-none mb-6  md:border-l  border-gray-100 md:rounded-e-lg md:shadow-md md:border  md:border-gray-100  ml-2 md:ml-0 mr-4  md:w-40'>
+          <input 
+            type="button"
+            className='h-12  w-full md:w-32 md:ml-3 md:mr-2 mt-2 cursor-pointer bg-green-700 rounded-lg text-white font-inter '
+            aria-label='search'
+            value='Schedule Now'
+          />
+        </div>
+      
+      </form>
     </div>
-
+    
     <div className='mb-10 md:mb-20 border border-green-700 mr-10 ml-10 md:ml-28 md:mr-20  md:min-h-72 min-h-32 rounded '>
       <div className='relative overflow-hidden'>
         <img className='md:h-72 h-40 w-full object-cover' src={frame} alt=""/>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,27 @@
 import { NavLink } from 'react-router-dom';
 import travlogo from '../assets/travlogo(2).svg'
 import MenuIcon from '@mui/icons-material/Menu';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import DesktopMacIcon from '@mui/icons-material/DesktopMac';
+import LogoutIcon from '@mui/icons-material/Logout';
 import { useState } from 'react';
 
 export default function Header() {
+
+  const user = true;
+
   const [toggle, setToggle] = useState(false);
 
   const toggleNavbar = ()=>{
     setToggle(!toggle);
   };
 
+  const [userToggle, setUserToggle] =useState(false);
+
   return (
     <>
       <div 
-        className={`${toggle ? 'h-[350px]' :'h-0'} space-y-6 space-x-8 md:space-x-0 md:space-y-0  flex-col md:h-20 shadow-md text-green-800 font-inter flex md:flex-row md:items-center  md:justify-around border-gray-100 border-2 border-solid`}>
+        className={`${toggle ? 'h-[350px]' :'h-0'} sticky right-0 left-0 top-0 z-50 space-y-6 space-x-8 md:space-x-0 md:space-y-0   flex-col md:h-20 shadow-md text-green-800 font-inter flex md:flex-row md:items-center  md:justify-around border-gray-100 border-2 border-solid text-sm`}>
           
         <div className='flex ml-8 mt-8 md:m-0 items-center'>
           <img                                                                               
@@ -30,13 +38,47 @@ export default function Header() {
 
         <div className={`${toggle ? 'flex' :'hidden'} md:flex flex-col space-y-4 md:space-y-0 md:flex-row md:none md:space-x-10 font-inter font-medium`}>
           <NavLink className='border-solid border-b-4 border-green-700 w-10' to='/'>Home</NavLink>
-          <a href="#">Our Service</a>
-          <a href="#">Contact Us</a>
+          <a href="#services">Our Service</a>
+          <a href="#Contact-us">Contact Us</a>
         </div>
 
-        <div className={` ${toggle ? 'flex' :'hidden'} md:flex flex-col space-y-4 w-20 md:w-44 md:flex-row md:items-center md:space-y-0 md:none md:space-x-6`}>
+        <div className={` ${toggle ? 'flex' :'hidden'} md:flex flex-col space-y-4 w-20 {}md:w-44 md:flex-row md:items-center md:space-y-0 md:none md:space-x-6`}>
+        {
+        user ? 
+        <div className='flex items-center space-x-1 relative cursor-pointer' onClick={()=> setUserToggle(!userToggle)}>
+          <PersonOutlineIcon fontSize='small'/>
+          <h1 className='font-inter text-sm'>Muhammad</h1>
+        {
+            userToggle ?
+          <div className='hidden absolute -bottom-[110px] space-y-4 bg-white rounded border border-gray-100  p-4'>
+            <div className='flex space-x-2 '>
+              <DesktopMacIcon fontSize='small'/>
+              <span>Dashboard</span>
+            </div>
+            <div className='space-x-2'>
+              <LogoutIcon fontSize='small'/>
+              <span>Logout</span>
+            </div>
+          </div>
+          :
+          <div className='absolute -bottom-[110px] space-y-4 bg-white rounded border border-gray-100  p-4'>
+          <div className='flex space-x-2 '>
+            <DesktopMacIcon fontSize='small'/>
+            <span>Dashboard</span>
+          </div>
+          <div className='space-x-2'>
+            <LogoutIcon fontSize='small'/>
+            <span>Logout</span>
+          </div>
+        </div>
+        }
+        </div>
+        :
+        <>
           <NavLink className='bg-green-700 text-white font-inter p-2 rounded' to='/login'>Log in</NavLink>
           <NavLink className='font-inter' to='/signup'>Sign Up</NavLink>
+        </>
+        }
         </div>
       </div>
     </>
